b9a2a13257b8db33680924b5c9f025b160b96080
BAaboe <benjamin.mjaatvedt@gmail.com>
2022-06-09 11:21:10 +0200

    added the ability and started on sending mail

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 from imapc import imapc
-import os
-import termcolor
-import base64
-import os
+from smtpc import smtpc
 import PIL.Image as Image
-import io
+import cv2, io, os, base64, termcolor
+import numpy as np
+from tqdm import tqdm
 
 class mcli:
     def __init__(self):
         self.imapc = imapc("mail.baaboe.fun", 993)
+        self.smtpc = imapc("mail.baaboe.fun", 25)
+        self.mail = ""
+        self.password = ""
 
     def clear(self):
         os.system("clear")
@@ -18,11 +20,11 @@ class mcli:
         if attachment["encoding"] == "base64":
             imgData = base64.decodebytes(attachment["data"].encode())
 
-        path = f"{os.path.expanduser('~')}/TestDownload/{attachment['fileName']}"
+        path = f"{os.path.expanduser('~')}/Downloads/{attachment['fileName']}"
         i = 1
         while True:
             if os.path.exists(path):
-                path = f"{os.path.expanduser('~')}/Download/({i}){attachment['fileName']}"
+                path = f"{os.path.expanduser('~')}/Downloads/({i}){attachment['fileName']}"
                 i += 1
             else: break
 
@@ -71,7 +73,65 @@ class mcli:
                         self.clear()
                 except ValueError:
                     self.clear()
+    
+    def show_attachment(self, c):
+        attachments_list = []
+        for content in c:
+            if content["type"].startswith("image"):
+                attachments_list.append(content)
+        self.clear()
+        while True:
+            for i, a in enumerate(attachments_list):
+                print(f"{i+1}. {a['fileName']}")
+            
+            print(termcolor.colored("--------------------------------", "blue"))
+            print("d. Download attachments")
+            print("b. Go back")
+            print("q. Quit")
 
+            choice = input(termcolor.colored("> ", "green")).lower()
+
+            if choice == "q":
+                quit()
+            elif choice == "b":
+                return
+            elif choice == "d":
+                self.download_attachment(c)
+                self.clear()
+            else:
+                try:
+                    if int(choice) > 0 and int(choice) < len(attachments_list)+1:
+                        index = int(choice)
+                        attachment = attachments_list[index-1]
+
+                        imgData = ""
+                        if attachment["encoding"] == "base64":
+                            imgData = base64.decodebytes(attachment["data"].encode())
+
+                        nparr = np.fromstring(imgData, np.uint8)
+                        image = cv2.imdecode(nparr, -1)
+
+                        cv2.imshow(attachment["fileName"], image)
+                        while True:
+                            k = cv2.waitKey(10)
+                            if k == 27:
+                                cv2.destroyAllWindows()
+                                break
+
+                        self.clear()
+                    else:
+                        self.clear()
+                except ValueError:
+                    self.clear()
+
+    def check_for_attachments(self, content):
+        i = 0
+        for c in content:
+            if c["type"].startswith("image"):
+                i += 1
+            elif c["type"].startswith("video"):
+                i += 1
+        return i
 
     def see_mail(self, index):
         refresh = True
@@ -83,24 +143,22 @@ class mcli:
                 mailDict, content = self.imapc.parseHeader(mail)
                 mailDict["content"] = self.imapc.parseContent(content, mail)
                 refresh = False
+            num_attachments = self.check_for_attachments(mailDict["content"])
             print(f"From: {mailDict['from']['name']}<{mailDict['from']['mail']}>" if mailDict['from']['name'] else {mailDict['from']['mail']})
             print(f"Subject: {mailDict['subject']}")
             print(f"Date: {mailDict['date']}")
+            print(termcolor.colored(f"Number of attachments: {num_attachments}", "red")) if num_attachments > 0 else print(f"Number of attachments: None")
             print(termcolor.colored("--------------------------------", "blue"))
             for c in mailDict["content"]:
                 if c["type"] == "text/plain":
                     if c["encoding"] == "base64":
-                        print("encodign shit")
+                        print(base64.decodebytes(c["data"].encode()))
                     else:
                         print(c["data"])
-                elif c["type"].startswith("image"):
-                    print(termcolor.colored("----------------", "blue"))
-                    print(termcolor.colored("There is a image attached", "red"))
-                    attachments = True
             print(termcolor.colored("--------------------------------", "blue"))
             print("n. Next mail")
             if index > 0: print("p. Previous mail")
-            if attachments: print("d. Download attachments")
+            if num_attachments > 0: print("s. Show attachments")
             print("b. Go back")
             print("q. quit")
             choice = input(termcolor.colored("> ", "green")).lower()
@@ -114,8 +172,8 @@ class mcli:
                 index -= 1
             elif choice == "b":
                 return
-            elif choice == "d" and attachments:
-                self.download_attachment(mailDict["content"])
+            elif choice == "s" and num_attachments > 0:
+                self.show_attachment(mailDict["content"])
 
 
     def checkInbox(self):
@@ -125,7 +183,8 @@ class mcli:
             self.clear()
             if refresh:
                 headerList = []
-                for i in range(10):
+                print(termcolor.colored("Fetching mails", "green"))
+                for i in tqdm(range(10)):
                     mail = self.imapc.getMail(i+seeing-10)
                     mailDict, content = self.imapc.parseHeader(mail)
                     name = ""
@@ -135,6 +194,7 @@ class mcli:
                         name = mailDict["from"]["mail"]
                     headerList.append({"out": f"Subject: {mailDict['subject']}, From: {name}'", "index": i+seeing-10})
                     refresh = False
+            self.clear()
             for i in headerList:
                 print(f"{i['index']+1}. {i['out']}")
             print(termcolor.colored("----------------------------------------------------------------", "blue"))
@@ -168,22 +228,34 @@ class mcli:
                 except ValueError:
                     continue
 
+    def send_mail(self):
+        pass
 
 
     def main(self):
         self.clear()
         while True:
-            if self.imapc.login():
-                break
+            email = input("Enter your email address: ")
+            password = input("Enter your password: ")
+            if "@" not in email:
+                self.clear()
+                print("Not a valid email address")
+            else:
+                if self.imapc.login(email, password):
+                    self.mail = email
+                    self.password = password
+                    break
+                
         while True:
             self.clear()
             print("1. Check Inbox")
+            print("2. Send Mail")
             print("q. Quit")
-            print("More coming soon")
             choice = input(termcolor.colored("> ", "green")).lower()
             if choice == "1":
-
                 self.checkInbox()
+            elif choice == "2":
+                self.send_mail()
             elif choice.lower() == "q":
                 quit()
             else:
